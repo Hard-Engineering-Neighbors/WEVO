9f4b351b7a251a19d16df5674e3c6d502ccdebe1
hul20 <jullianbilan20@gmail.com>
2025-05-03 21:35:33 +0800

    feat: landing page

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import "./index.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-between text-center p-6">
+      {/* Main content */}
+      <div className="flex-grow flex flex-col items-center justify-center space-y-4">
+        {/* Logo */}
+        <img
+          src="public\wevoLogoPng.png"
+          alt="Wevo Logo"
+          className="w-auto max-h-40 mb-4"
+        />
+
+        {/* Tagline */}
+        <div className="text-4xl text-blue-600 font-semibold">
+          Smart Scheduling for a Smarter Campus
+        </div>
+
+        {/* Paragraph */}
+        <p className="text-base max-w-3xl text-gray-500">
+          Keeps West Visayas State University in sync by providing real-time
+          updates on venue availability and streamlining communication between
+          student organizations and campus administration.
+        </p>
+
+        {/* Buttons */}
+        <div className="flex space-x-4 mt-4">
+          <button className="bg-blue-600 text-white px-10 py-2 rounded-full hover:bg-blue-700 transition">
+            Get Started &#10148;
+          </button>
+          <button className="border border-blue-600 text-blue-600 px-10 py-2 rounded-full hover:bg-blue-50 transition">
+            Explore
+          </button>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="text-sm text-gray-400 pb-4">
+        <p className="font-bold">
+          All Rights Reserved: Hard Engineering Neighbors{" "}
+        </p>
+        <p>
+          WEVO is an an interactive web application designed to simplify venue
+          reservations and provide a real-time monitoring of events.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
